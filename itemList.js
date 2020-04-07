0908cf98094e6db1f6b5006b5cdb8ac9854e6a96
Chaoyu <cyxiang@students.cs.ubc.ca>
2020-04-07 01:00:49 -0700

    add render

--- itemList.js
+++ itemList.js
@@ -1,6 +1,7 @@
 document.querySelector('#submit').addEventListener('click', (e) => {
     e.preventDefault();
     document.querySelector('.errorLable').innerHTML = '';
+    document.querySelector('.renderItem').innerHTML = '';
     let newItem = document.querySelector('#newItem');
     try {
         let item = generateItem(newItem);
@@ -29,12 +30,23 @@ const generateItem = function (newItem) {
 };
 
 const renderItem = function (item) {
-    
+    const title = document.createElement('h4');
+    title.textContent = "Here is the result";
+    const renderResult = document.createElement('select');
+    renderResult.className = "itemList";
+    const oneItem = document.createElement('option');
+    oneItem.text = `${item.cate}  :  Spent  ${item.money} $  on  ${item.date}  for  ${item.name}`;
+    renderResult.options.add(oneItem,1);
+    document.querySelector('.renderItem').appendChild(title);
+    document.querySelector('.renderItem').appendChild(renderResult);
 }
 
 const renderError = function (e) {
+    let title = document.createElement('h4');
+    title.textContent = "Error : "
     let errorChild = document.createElement('p');
     errorChild.className = 'error';
     errorChild.textContent = e.message;
+    document.querySelector('.errorLable').appendChild(title);
     document.querySelector('.errorLable').appendChild(errorChild);
 }
