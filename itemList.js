aca0831cb2f18710c06b8e9f41bd7837d53e32be
Chaoyu <cyxiang@students.cs.ubc.ca>
2020-04-06 23:58:26 -0700

    split into 2 parts

--- itemList.js
+++ itemList.js
@@ -1,32 +1,40 @@
-document.querySelector('#submit').addEventListener('click',(e)=>{
+document.querySelector('#submit').addEventListener('click', (e) => {
     e.preventDefault();
     document.querySelector('.errorLable').innerHTML = '';
     let newItem = document.querySelector('#newItem');
-    let itemName = newItem.querySelector('.itemName').value;
+    try {
+        let item = generateItem();
+        renderItem(item);
+    }
+    catch (e) {
+        renderError(e);
+    }
+});
+
+const generateItem = function (newItem) {
+    let name = newItem.querySelector('.itemName').value;
     let money = newItem.querySelector('.itemMoney').value;
     let cate = newItem.querySelector('.dropdown').value;
     let date = newItem.querySelector('.date').value;
-    try {
-        let item = generateItem(itemName,money,cate,date);
-        console.log(item);
-    }
-    catch(e){
-        let errorChild = document.createElement('p');
-        errorChild.className = 'error';
-        errorChild.textContent = e.message;
-        document.querySelector('.errorLable').appendChild(errorChild);
-    }   
-});
-
-const generateItem = function(name,money,cate,date) {
     let item = {};
-    item['name'] = name;
     let moneyNum = parseInt(money);
-    if(moneyNum < 0){
+    if (moneyNum < 0) {
         throw new Error('Money value is less than 0 !');
     }
+    item['name'] = name;
     item['money'] = parseInt(money);
     item['cate'] = cate;
     item['date'] = date;
     return item;
 };
+
+const renderItem = function (item) {
+    
+}
+
+const renderError = function (e) {
+    let errorChild = document.createElement('p');
+    errorChild.className = 'error';
+    errorChild.textContent = e.message;
+    document.querySelector('.errorLable').appendChild(errorChild);
+}
